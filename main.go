e9273a2648f2d89b4a237ff1f004ba4482b6f715
xblymmx <tsmwymsmdyts@gmail.com>
2024-09-29 18:19:55 +0800

    init

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	ginzap "github.com/gin-contrib/zap"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+const logPath = "./logs/go.log"
+
+var logger *zap.Logger
+
+func setupLog() {
+	os.OpenFile(logPath, os.O_RDONLY|os.O_CREATE, 0666)
+	c := zap.NewProductionConfig()
+	c.OutputPaths = []string{"stdout", logPath}
+	var err error
+	logger, err = c.Build()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	setupLog()
+
+	r := gin.Default()
+
+	// Setting GIN to use zap as logger
+	r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
+	r.Use(ginzap.RecoveryWithZap(logger, true))
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
